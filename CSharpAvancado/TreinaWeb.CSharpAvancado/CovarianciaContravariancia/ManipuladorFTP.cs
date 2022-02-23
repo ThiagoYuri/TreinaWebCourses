72a69a6f6c4345468424000eac70bd80b79a673e
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-02-23 11:33:25 -0300

    Using Interfaces gerenerics

---
+++ CSharpAvancado/TreinaWeb.CSharpAvancado/CovarianciaContravariancia/ManipuladorFTP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianciaContravariancia
+{
+    public class ManipuladorFTP<T> : IArmazenador<T>, IRecuperador<T>
+    {
+        private List<T> _lista = new List<T>();
+        public void Armazenar(T item)
+        {
+            _lista.Add(item);
+        }
+
+        public T Recuperar(int codigo)
+        {
+            return _lista[codigo];
+        }
+
+    }
+}
---
+++ CSharpAvancado/TreinaWeb.CSharpAvancado/CovarianciaContravariancia/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CovarianciaContravariancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManipuladorFTP<string> ftp = new ManipuladorFTP<string>();
+
+            ftp.Armazenar("teste");
+            Console.WriteLine(ftp.Recuperar(0));
+            Console.ReadKey();
+        }
+    }
+}
